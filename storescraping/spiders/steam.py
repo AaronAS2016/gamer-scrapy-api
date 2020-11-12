238ecadccc03d5ebfc8be3ef530a75c4772f8657
Aaron Saban <aaronsaban.as2012@gmail.com>
2020-11-12 20:06:22 -0300

    feature: agregado categoria en steam :D

--- storescraping/spiders/steam.py
+++ storescraping/spiders/steam.py
@@ -1,5 +1,6 @@
 import scrapy
-
+import re
+from scrapy import FormRequest, Request
 from storescraping.utils.url_builder import url_search_build
 from storescraping.sites.modes import Validador
 from storescraping.config.constant import SIN_PRECIO
@@ -9,8 +10,8 @@ class SteamSpider(scrapy.Spider, Validador):
     name = "steam"
 
     selector_items = "//div[@id='search_resultsRows']/a"
-    selector_price = ".search_price::text"
-    selector_title = ".//span[@class='title']/text()"
+    selector_price = ".game_purchase_price::text"
+    selector_title = ".//div[@class='apphub_AppName']/text()"
 
     def __init__(self, query, modo, url_search, *args, **kwargs):
         super(SteamSpider, self).__init__(*args, **kwargs)
@@ -19,25 +20,24 @@ class SteamSpider(scrapy.Spider, Validador):
         self.query = query
         self.modo = self.obtener_modo(modo)
 
+
+    def parse_product(self, response):
+        category = re.sub(r'[\r|\n|\t]', '', str(response.css("a.app_tag::text")[0].get()))
+        title = response.xpath(self.selector_title).get()
+        price = re.sub(r'[\r|\n|\t]', '', str(response.css(self.selector_price).get()))
+
+        if self.modo(self.query, title.lower()):
+            yield {
+                "title": title,
+                "price": price,
+                "provider": self.name,
+                "link": response.url,
+                "category": category
+            }
+
     def parse(self, response):
         items = response.xpath(self.selector_items)
 
         for item in items:
-            title: str = item.xpath(self.selector_title).get()
-            price: int = item.css(self.selector_price).get().strip()
-
-            if self.modo(self.query, title.lower()):
-                self.contador += 1
-                if not price:
-                    try:
-                        price = item.css(self.selector_price).getall()[
-                            1].strip()
-                    except IndexError:
-                        price = SIN_PRECIO
-                    
-                if price != SIN_PRECIO:
-                    yield {
-                        "title": title,
-                        "price": price,
-                        "provider": self.name
-                    }
+            link = item.xpath("./@href").get()
+            yield response.follow(link, callback=self.parse_product, cookies={'lastagecheckage': '1-0-1931', 'birthtime' : "-539125199"})
